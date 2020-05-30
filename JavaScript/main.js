9a334ad3f3d0f428d2bab80bb6564c3be06bb408
Hasitha <ranaweerahlakshan@gmail.com>
2020-05-30 14:48:26 +0530

    Added all login requirements for Js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,41 @@
     var username = document.getElementById("uname");
     var password = document.getElementById("pword");
+    var login_form = document.getElementById("login_form");
 
 
     function validate()
     {
         this.username = username;
         this.password = password;
+        this.login_form = login_form;
 
         if((username.value =="admin" && password.value =="123") || (username.value =="analyzer" && password.value =="123") || (username.value =="cash collector" && password.value =="123") || (username.value =="sales manager" && password.value =="123") || (username.value =="inventory clerk" && password.value =="123"))
         {
+            if(username.value =="admin")
+            {
+                login_form.action = "#admin_dashboard";
+            }
+
+            else if(username.value =="analyzer")
+            {
+                login_form.action = "#analyzer_dashboard";
+            }
+
+            else if(username.value =="cash collector")
+            {
+                login_form.action = "#cash_collector_dashboard";
+            }
+
+            else if(username.value =="sales manager")
+            {
+                login_form.action = "#sales_manager_dashboard";
+            }
+
+            else if(username.value =="inventory clerk")
+            {
+                login_form.action = "#inventory_cleark_dashboard";
+            }
+
             return true;
         }
 
