3ae893eb71342128e7d4aaf6970befd5b5267f47
Hasitha <ranaweerahlakshan@gmail.com>
2020-05-30 13:42:08 +0530

    Created login for all users

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,33 +1,37 @@
-function validate()
-{
     var username = document.getElementById("uname");
     var password = document.getElementById("pword");
 
-    if(username.value =="admin" && password.value =="123")
-    {
-        return true;
-    }
 
-    else if(username.value =="admin" && password.value !="123")
+    function validate()
     {
-        alert("Incorrect Password or Username");
-        password.style.border = "1px solid red";
-        username.style.border = "1px solid red";
-        return false;
-    }
+        this.username = username;
+        this.password = password;
 
-    else if(username.value !="admin" && password.value =="123")
-    {
-        alert("Incorrect Password or Username");
-        username.style.border = "1px solid red";
-        password.style.border = "1px solid red";
-        return false;
-    }
+        if((username.value =="admin" && password.value =="123") || (username.value =="analyzer" && password.value =="123") || (username.value =="cash collector" && password.value =="123") || (username.value =="sales manager" && password.value =="123") || (username.value =="inventory clerk" && password.value =="123"))
+        {
+            return true;
+        }
 
-    else{
-        alert("Please Enter Correct Details");
-        username.style.border = "1px solid red";
-        password.style.border = "1px solid red";
-        return false;
+        else if((username.value =="admin" && password.value !="123") || (username.value =="analyzer" && password.value !="123") || (username.value =="cash collector" && password.value !="123") || (username.value =="sales manager" && password.value !="123") || (username.value =="inventory clerk" && password.value !="123"))
+        {
+            alert("Incorrect Password or Username");
+            password.style.border = "1px solid red";
+            username.style.border = "1px solid red";
+            return false;
+        }
+
+        else if((username.value !="admin" && password.value =="123") || (username.value !="analyzer" && password.value =="123") || (username.value !="cash collector" && password.value =="123") || (username.value !="sales manager" && password.value =="123") || (username.value !="inventory clerk" && password.value =="123"))
+        {
+            alert("Incorrect Password or Username");
+            username.style.border = "1px solid red";
+            password.style.border = "1px solid red";
+            return false;
+        }
+
+        else{
+            alert("Please Enter Correct Details");
+            username.style.border = "1px solid red";
+            password.style.border = "1px solid red";
+            return false;
+        }
     }
-}
